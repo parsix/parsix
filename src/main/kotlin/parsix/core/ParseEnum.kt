305a1a8aec0ccd879b80aba9812dab520dcf3f2a
spell <pelligra.s@gmail.com>
2021-05-13 15:22:02 +0200

    improve ParseEnum
    Signed-off-by: spell <pelligra.s@gmail.com>

--- src/main/kotlin/parsix/core/ParseEnum.kt
+++ src/main/kotlin/parsix/core/ParseEnum.kt
@@ -1,6 +1,8 @@
 package parsix.core
 
 /**
+ * Models an Enum that can be parsed
+ *
  * @see parseEnum
  */
 interface ParsableEnum {
@@ -10,20 +12,18 @@ interface ParsableEnum {
 /**
  * @see parseEnum
  */
-data class EnumError(val expected: Set<String>) : OneError()
+data class EnumError(val value: String, val expected: Set<String>) : OneError()
 
 /**
  * Make a `parse` for Enum [T].
  * The returned parse we will match string value against [ParsableEnum.key]
  * and return [EnumError] in case of failure
+ *
+ * @sample parsix.core.ParseEnumKtTest
  */
 inline fun <reified T> parseEnum(): Parse<String, T>
     where T : Enum<T>, T : ParsableEnum {
-    val map = mapOf(
-        *enumValues<T>()
-            .map { enum -> enum.key to enum }
-            .toTypedArray()
-    )
+    val map = enumValues<T>().associateBy { it.key }
 
-    return { inp -> map[inp]?.let(::Ok) ?: EnumError(map.keys) }
+    return { inp -> map[inp]?.let(::Ok) ?: EnumError(inp, map.keys) }
 }
